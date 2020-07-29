8cd7549cc153cf82943220d6ff2af4354c3667a3
Nguyen Dang Nguyen <dangnguyendota@gmail.com>
2020-07-29 17:22:43 +0700

    add game loop and game state

--- room_handler.go
+++ room_handler.go
@@ -12,11 +12,18 @@ type RoomData struct {
 }
 
 type RoomHandler interface {
-	OnInit(room Room)
+	// init the game and return game state
+	OnInit(room Room) interface{}
+	// check join condition for allow or disallow a user join this room
 	AllowJoin(room Room, user *User) *CheckJoinConditionResult
+	// when scheduled an action, this action will jump here
 	Processor(room Room, action string, data map[string]interface{})
-	OnJoined(room Room, user *User)
-	OnLeft(room Room, user *User)
-	Loop(room Room, roomChan <-chan *RoomData)
+	// when a user joined this room (players only) and return the game state
+	OnJoined(room Room, user *User) interface{}
+	// when a player left room (or disconnected) (players only) and return the game state
+	OnLeft(room Room, user *User) interface{}
+	// handle all queued messages from clients and return the game state
+	Loop(room Room, roomChan <-chan *RoomData) interface{}
+	// when room is closed
 	OnClose(room Room)
 }
