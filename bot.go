aeca970ba76f9a96e7e1490f5bcf4c6b057c61cf
Nguyen Dang Nguyen <dangnguyendota@gmail.com>
2021-03-17 14:40:15 +0700

    xoa remove bot (dung Kick trong Room thay the)

--- bot.go
+++ bot.go
@@ -6,13 +6,7 @@ import "github.com/google/uuid"
 //  Bot là các người chơi ảo dùng để giả làm người trong game
 type Bot interface {
 	// hàm Get gọi Bot vào trong bàn
-	// nếu gọi Bot thành công sẽ trả về callback chứa ID của Bot
-	// returns error nếu gọi Bot thất bại
-	Get(callback func(botId uuid.UUID)) error
-	// hàm Remove xóa (đuổi) Bot khỏi Room
-	// nếu xóa Bot thành công thì sẽ trả về 1 callback chứa ID của Bot bị kick
-	// returns error nếu xóa Bot thất bại
-	Remove(botId uuid.UUID, callback func(botId uuid.UUID)) error
+	Get(callback func(err error))
 	// hàm Call sẽ gửi 1 request lên Bot server
 	// sau khi Bot server nhận được request sẽ xử lý request đó và trả về
 	// một response trong hàm callback
