292b65a6bd78318f6327f43f2081dc6ba7d82eb6
Nguyen Dang Nguyen <dangnguyendota@gmail.com>
2021-03-18 10:33:47 +0700

    them context cho bot

--- bot.go
+++ bot.go
@@ -1,15 +1,17 @@
 package api
 
-import "github.com/google/uuid"
+import (
+	"context"
+)
 
 // Mỗi Room đều chứa 1 Bot dùng để làm việc với Bot được viết sẵn trước đó
 //  Bot là các người chơi ảo dùng để giả làm người trong game
 type Bot interface {
 	// hàm Get gọi Bot vào trong bàn
-	Get(callback func(err error))
+	Get(ctx context.Context, callback func(err error))
 	// hàm Call sẽ gửi 1 request lên Bot server
 	// sau khi Bot server nhận được request sẽ xử lý request đó và trả về
 	// một response trong hàm callback
 	// returns error nếu call Bot thất bại
-	Call(botId uuid.UUID, request string, callback func(response string)) error
+	Call(ctx context.Context, request string, callback func(response string)) error
 }
